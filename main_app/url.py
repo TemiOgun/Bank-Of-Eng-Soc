be7073d4468db7e2bfb5424a07fd90b538050af9
Temi Ogunsanya <13too1@queensu.ca>
2016-12-24 19:02:46 -0500

    login page

--- main_app/url.py
+++ main_app/url.py
@@ -1,5 +1,13 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
+url('^', include('django.contrib.auth.urls'))
+#url('^change-password/$', auth_views.PasswordChangeView.as_view()),
+# url(
+        # '^change-password/$',
+        # auth_views.PasswordChangeView.as_view(template_name='change-password.html'),
+    # ),
+
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
+from django.conf import settings
+from django.shortcuts import redirect
+
 
 # Create your views here.
+def my_view(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        # Redirect to a success page.
+        
+	else:
+		print  "invalid login details " + username + " " + password
+        return render_to_response('login.html', {}, context)
+
+		
+def logout_view(request):
+    logout(request)
+    # Redirect to a success page.
+	
+def my_view(request):
+    if not request.user.is_authenticated:
+        return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
+    # ...
