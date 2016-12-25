112cb563d4ffc3dfd5c885fd9b44345776e5147d
Temi Ogunsanya <13too1@queensu.ca>
2016-12-25 18:35:02 -0500

    edited Login HTML/CSS, added logo to main page

--- main_app/views.py
+++ main_app/views.py
@@ -5,25 +5,16 @@ from django.conf import settings
 from django.shortcuts import redirect
 
 
-# Create your views here.
+
 def my_view(request):
     username = request.POST['username']
     password = request.POST['password']
-    user = authenticate(request, username=username, password=password)
+    user = authenticate(username=username, password=password)
     if user is not None:
-        login(request, user)
+		login(request, user)
         # Redirect to a success page.
-        
-	else:
-		print  "invalid login details " + username + " " + password
-        return render_to_response('login.html', {}, context)
-
+    else:
+		print "log in failed"
+		return HttpResponse("Invalid login details supplied")
+        # Return an 'invalid login' error message.
 		
-def logout_view(request):
-    logout(request)
-    # Redirect to a success page.
-	
-def my_view(request):
-    if not request.user.is_authenticated:
-        return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
-    # ...
